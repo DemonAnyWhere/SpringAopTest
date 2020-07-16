5bd510bab55998b90174c37541e16ea993e531e5
LiRui <18640169382@163.com>
2020-07-16 12:44:48 +0800

    环绕通知执行顺序

--- src/main/java/com/lirui/aspect/LogUtil.java
+++ src/main/java/com/lirui/aspect/LogUtil.java
@@ -1,6 +1,8 @@
 package com.lirui.aspect;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.Signature;
 import org.aspectj.lang.annotation.*;
 import org.springframework.stereotype.Component;
 
@@ -9,7 +11,13 @@ import java.util.Arrays;
 @Aspect
 @Component
 public class LogUtil {
-    @Before("execution(* com.lirui.controller.MySpringAopController.*(..))")
+
+    @Pointcut("execution(* com.lirui.controller.MySpringAopController.*(..))")
+    public void myPointCut(){
+
+    }
+
+    @Before(value = "myPointCut()")
     public static void start(JoinPoint joinPoint){
         System.out.println(joinPoint.getSignature().getName() +"方法开始执行：参数是" + Arrays.asList(joinPoint.getArgs()));
     }
@@ -24,6 +32,27 @@ public class LogUtil {
     @After("execution(* com.lirui.controller.MySpringAopController.*(..))")
     public static void logFinally(){
         System.out.println("方法执行结束。。。。。over");
+    }
+    //环绕通知
+    /*
+      通知执行顺序：
+            环绕前置-->普通前置-->目标方法执行-->环绕正常结束/出现异常-->环绕后置-->普通后置-->普通返回或者异常。
+     */
+    @Around(value = "myPointCut()")
+    public Object around(ProceedingJoinPoint proceedingJoinPoint) {
+        Signature signature = proceedingJoinPoint.getSignature();
+        Object[] args = proceedingJoinPoint.getArgs();
+        Object result = null;
+        try {
+            System.out.println("环绕通知start:" + signature.getName() +"方法开始执行，参数为：" + Arrays.asList(args));
+            result = proceedingJoinPoint.proceed();
+            System.out.println("环绕通知stop:" + signature.getName() + "方法执行结束");
+        } catch (Throwable throwable) {
+            System.out.println("环绕异常通知" + signature.getName() + "出现异常");
+        } finally {
+            System.out.println("环绕返回通知" + signature.getName() + "方法返回结果是:" + result);
+        }
+        return result;
 
     }
 }
